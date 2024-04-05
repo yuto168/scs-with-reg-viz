1c5a8325f1b35a601917fdc638ead46c611645a9
yuto <kanawo202090@gmail.com>
2024-04-05 16:43:02 +0900

    テスト

--- src/Button.js
+++ src/Button.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export const Button = ({ children, primary }) => (
   <button
     style={{
-      display: 'inline-block',
-      padding: '0 3em',
+      display: "inline-block",
+      padding: "0 3em",
       height: 50,
       borderRadius: 50,
-      borderStyle: 'solid',
+      borderStyle: "solid",
       borderWidth: 1,
-      borderColor: primary ? '#00c4a7' : '#adadad',
-      background: primary ? '#00c4a7' : '#fff',
-      color: primary ? '#fff' : '#363636',
-      font: 'normal 14px/50px sans-serif',
-      textRendering: 'optimizeLegibility',
+      borderColor: primary ? "#00c4a7" : "#red",
+      background: primary ? "#00c4a7" : "#fff",
+      color: primary ? "#fff" : "#363636",
+      font: "normal 14px/50px sans-serif",
+      textRendering: "optimizeLegibility",
     }}
   >
     {children}
